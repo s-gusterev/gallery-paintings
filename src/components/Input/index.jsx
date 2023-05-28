b58ae6ebbb79a852820fd574b2cafca0ace8893d
Sergei Gusterev <copywriter-sg@yandex.ru>
2023-05-28 05:20:54 +0300

    feat: input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
+import styles from './Input.module.css';
 
 const Input = ({ type, placeholder }) => {
-  return <input type={type} placeholder={placeholder} />;
+  return (
+    <input type={type} placeholder={placeholder} className={styles.input} />
+  );
 };
 
 Input.propTypes = {
