35160b3e915cfa731368c5c064fd1bef68fb42d1
Sergei Gusterev <copywriter-sg@yandex.ru>
2023-06-02 18:55:14 +0300

    feat: pagination component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import Pagination from './components/Pagination';
 function App() {
   return (
     <div className='container'>
       <Header />
       <Main />
+      <Pagination />
     </div>
   );
 }
