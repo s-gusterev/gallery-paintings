c5d11da963c3447a6629da35a9e92db62acfd81d
Sergei Gusterev <copywriter-sg@yandex.ru>
2023-06-17 08:45:16 +0300

    bufix: selected dropdownindicator

--- src/components/Selected/index.jsx
+++ src/components/Selected/index.jsx
@@ -15,7 +15,7 @@ const Selected = ({ options, placeholder, onChange }) => {
         >
           <path
             d='M9.67861 1.8337L5.77064 5.68539C5.34503 6.10487 4.65497 6.10487 4.22936 5.68539L0.321394 1.8337C-0.365172 1.15702 0.121082 -8.3659e-08 1.09203 0L8.90797 6.73452e-07C9.87892 7.57113e-07 10.3652 1.15702 9.67861 1.8337Z'
-            fill='var(--color-font-input)'
+            fill='var(--color-secondary-100)'
           />
         </svg>
       </components.DropdownIndicator>
@@ -146,10 +146,11 @@ const Selected = ({ options, placeholder, onChange }) => {
       ...defaultStyles,
       display: 'none',
     }),
-    dropdownIndicator: (defaultStyles) => ({
+    dropdownIndicator: (defaultStyles, state) => ({
       ...defaultStyles,
       padding: 0,
       marginRight: '18px',
+      transform: state.selectProps.menuIsOpen && 'rotate(180deg)',
     }),
     menu: (defaultStyles) => ({
       ...defaultStyles,
@@ -196,6 +197,7 @@ const Selected = ({ options, placeholder, onChange }) => {
         isSearchable={false}
         placeholder={placeholder}
         onChange={onChange}
+        // isClearable={true}
       />
     </div>
   );
